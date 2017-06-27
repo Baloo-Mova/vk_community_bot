8ee02a8d5a673da889aec2a3058d26799b4407cf
Baloo-Mova <sergious-91@mail.ru>
2017-06-27 20:09:46 +0300

    Update Provider

---
+++ database/migrations/2017_06_27_125902_add_user_groups.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserGroups extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_groups',function(Blueprint $table){
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('name');
+            $table->string('avatar')->nullable();
+            $table->integer('group_id');
+            $table->integer('expiries');
+            $table->string('token');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_groups');
+    }
+}
