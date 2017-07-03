b1a5ff3d15e7cad705fee1019cf6d5d670ce63a1
DESKTOP-47KA4EV\petly <petliak.serhii@gmail.com>
2017-07-03 18:35:14 +0300

    fix routes

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/login', "HomeController@login")->name('login');
 Route::post('/logout', "HomeController@logout")->name('logout');
 Route::get('/social_login/', 'SocialController@login')->name('vk_login');
 Route::get('/social_login/callback/', 'SocialController@loginCallback');
+Route::get('/check-result/', ['uses' => 'BalanceController@checkResult', 'as' => 'balance.check.result']);
+
 
 Route::group(['middleware' => ['vkAuth', 'auth']], function () {
     Route::get('/', "GroupsController@index")->name('groups.index');
@@ -26,7 +28,6 @@ Route::group(['middleware' => ['vkAuth', 'auth']], function () {
     Route::group(['prefix' => 'balance'], function () {
         Route::get('/', ['uses' => 'BalanceController@index', 'as' => 'balance.index']);
         Route::post('/replenishment', ['uses' => 'BalanceController@replenishment', 'as' => 'balance.replenishment']);
-        Route::get('/check-result/', ['uses' => 'BalanceController@checkResult', 'as' => 'balance.check.result']);
         Route::get('/check-success/', ['uses' => 'BalanceController@checkSuccess', 'as' => 'balance.check.success']);
         Route::get('/check-fair/', ['uses' => 'BalanceController@checkFair', 'as' => 'balance.check.fair']);
     });
