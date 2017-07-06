4dcb8ec9e81d7af79697370f76c87823ee4596dc
DESKTOP-47KA4EV\petly <petliak.serhii@gmail.com>
2017-07-06 13:11:27 +0300

    add robokassa config

---
+++ config/robokassa.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    "login" => "",
+    "password1" => "",
+    "password2" => "",
+    "demo_password1" => "",
+    "demo_password2" => "",
+    "community_one_month_price" =>
+];
