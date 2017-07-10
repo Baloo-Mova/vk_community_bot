7bdf864327dd9cde188ca8a77b92eef8080c0102
Baloo-Mova <sergious-91@mail.ru>
2017-07-10 22:29:36 +0300

    asd

--- app/Http/Controllers/VkListenerController.php
+++ app/Http/Controllers/VkListenerController.php
@@ -9,7 +9,7 @@ class VkListenerController extends Controller
     public function index(Request $request, $id)
     {
         $data = json_decode($request->getContent());
-
+        file_put_contents(storage_path('app/test.txt'), $request->getContent());
         if ($data['type'] == 'confirmation') {
             return "740cb521";
         }
