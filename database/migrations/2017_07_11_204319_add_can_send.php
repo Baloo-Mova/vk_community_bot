0937a1b9a1c045b44af0210eda046dcf5dbef841
Baloo-Mova <sergious-91@mail.ru>
2017-07-12 16:30:38 +0300

    qwe

--- database/migrations/2017_07_11_204319_add_can_send.php
+++ database/migrations/2017_07_11_204319_add_can_send.php
@@ -14,7 +14,7 @@ class AddCanSend extends Migration
     public function up()
     {
         Schema::table('clients', function (Blueprint $table) {
-            $table->boolean('can_send')->default(0);
+            $table->boolean('can_send')->default(1);
         });
     }
 
