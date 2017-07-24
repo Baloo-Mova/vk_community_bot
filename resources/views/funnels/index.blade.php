df70708d611138637621f5f73fe8e59a83971ad7
Baloo-Mova <sergious-91@mail.ru>
2017-07-24 13:33:03 +0300

    asd

--- resources/views/funnels/index.blade.php
+++ resources/views/funnels/index.blade.php
@@ -103,7 +103,7 @@
                                     <td class="col s2 funnels_td">
                                         <div>
                                             {{var_dump($funnel)}}
-                                            {{ $funnel->clientGroup->name}}
+                                            {{// $funnel->clientGroup->name}}
                                         </div>
                                     </td>
                                     <td class="col s2 funnels_td">
