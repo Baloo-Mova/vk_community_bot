f9cac035d96c4c2e1cc14657be6f682026a76489
Baloo-Mova <sergious-91@mail.ru>
2017-07-24 20:34:37 +0300

    sdasd

--- app/Http/Controllers/MassDeliveryController.php
+++ app/Http/Controllers/MassDeliveryController.php
@@ -85,7 +85,7 @@ class MassDeliveryController extends Controller
         if (empty($then_send)) {
             $delivery->when_send = Carbon::now('Europe/Moscow');
         } else {
-            $delivery->when_send = Carbon::createFromFormat("Y-m-d H:i:s", $then_send);
+            $delivery->when_send = Carbon::createFromFormat("d-m-Y H:i", $then_send);
         }
         $delivery->save();
 
