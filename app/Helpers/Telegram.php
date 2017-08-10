069e510ab25b9002af9f4b941767f15d2cca01f4
solomon <sergious-91@mail.ru>
2017-08-10 14:19:42 +0300

    qwe

--- app/Helpers/Telegram.php
+++ app/Helpers/Telegram.php
@@ -15,35 +15,35 @@ class Telegram
     public function __construct()
     {
         $this->client = new Client([
-            'headers'         => [
-                'User-Agent'      => 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36 OPR/41.0.2353.69',
-                'Accept'          => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'headers' => [
+                'User-Agent' => 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36 OPR/41.0.2353.69',
+                'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
                 'Accept-Encoding' => 'gzip, deflate, lzma, sdch, br',
                 'Accept-Language' => 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
             ],
-            'verify'          => false,
-            'cookies'         => true,
+            'verify' => false,
+            'cookies' => true,
             'allow_redirects' => true,
-            'timeout'         => 15
+            'timeout' => 15
         ]);
     }
 
     public function sendMessage($group_id, $text)
     {
-        if ( ! isset($group_id) || ! isset($text)) {
+        if (!isset($group_id) || !isset($text)) {
             return false;
         }
 
         $group = UserGroups::find($group_id);
 
-        if(!isset($group)){
+        if (!isset($group)) {
             return false;
         }
 
         $chat_id = $group->telegram;
         $telegram_token = config('telegram.token');
 
-        if(!isset($telegram_token)){
+        if (!isset($telegram_token)) {
             return false;
         }
 
@@ -68,9 +68,9 @@ class Telegram
         try {
             $request = $this->client->request("GET",
                 "https://api.telegram.org/bot" . $this->bot->bot_key . "/getUpdates?offset=" . $this->bot->offset);
-            $data    = json_decode($request->getBody()->getContents());
+            $data = json_decode($request->getBody()->getContents());
 
-            if ( ! empty($data) && $data->ok) {
+            if (!empty($data) && $data->ok) {
                 $results = $data->result;
             } else {
                 $results = [];
@@ -82,7 +82,7 @@ class Telegram
 
                     $group = UserGroups::where(['telegram_keyword' => $item->message->text])->first();
 
-                    if(!isset($group)){
+                    if (!isset($group)) {
                         continue;
                     }
 
@@ -90,7 +90,7 @@ class Telegram
                     $group->save();
 
                     $this->client->request("GET",
-                        "https://api.telegram.org/bot" . config('telegram.token') . "/sendMessage?chat_id=" . $item->message->chat->id . "&text=Вы успешно привязали Telegram. Ваш TelegramId - ".$item->message->chat->id);
+                        "https://api.telegram.org/bot" . config('telegram.token') . "/sendMessage?chat_id=" . $item->message->chat->id . "&text=Вы успешно привязали Telegram к группе: \"" . $group->name . "\"");
 
                 }
             }
