52bc3a0e37762429f655cf536623e2029ae11465
solomon <sergious-91@mail.ru>
2017-08-30 14:27:30 +0300

    qwe

--- app/Providers/VK/SocialiteProvider.php
+++ app/Providers/VK/SocialiteProvider.php
@@ -126,7 +126,7 @@ class SocialiteProvider extends AbstractProvider implements ProviderInterface
 
         if (is_null($this->httpClient)) {
             $this->httpClient = new Client([
-                'proxy'  => '194.28.210.3:8000',
+               // 'proxy'  => '194.28.210.3:8000',
                 'verify' => false,
             ]);
         }
